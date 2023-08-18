41c58cf5ef5e52fcac9395d794d4474691ac6a0e
Anaëlle Touzet <contact@anaelle.dev>
2023-08-18 15:41:26 +0200

    Upadted recette related files
    Added dark mode etc... 😴

--- scripts/recette.js
+++ scripts/recette.js
@@ -1,6 +1,7 @@
 // Vérifier si l'appareil est un mobile
 function isMobileDevice() {
-  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  const isMobile = window.matchMedia("(max-width: 767px)").matches;
+  return isMobile;
 }
 
 // Ajouter l'effet glitch au survol pour les PC
@@ -48,3 +49,43 @@ setTimeout(function () {
 setTimeout(function () {
   glitchTitle.classList.add('glitch-hover');
 }, 10000); // Démarrer l'effet glitch après 10 secondes sur mobile à nouveau
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+// Check for the presence of the dark mode cookie
+const darkModeCookie = getCookie('darkMode');
+if (darkModeCookie === 'true') {
+  darkModeToggle.checked = true;
+  body.classList.add('dark-mode');
+}
+
+// Add event listener to the dark mode toggle
+darkModeToggle.addEventListener('change', () => {
+  body.classList.toggle('dark-mode');
+  setCookie('darkMode', darkModeToggle.checked);
+});
+
+// Function to set a cookie
+function setCookie(name, value, days = 365) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Function to get a cookie
+function getCookie(name) {
+  const cookieName = name + "=";
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return "";
+}
